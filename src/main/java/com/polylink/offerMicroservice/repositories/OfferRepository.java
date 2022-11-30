b013b9c37f03f652087a3bf9fc3a9b56fcb31b27
Alexandre <alexandre.fernique@etu.umontpellier.fr>
2022-11-30 20:38:41 +0100

    Fix create update and delete offer

--- src/main/java/com/polylink/offerMicroservice/repositories/OfferRepository.java
+++ src/main/java/com/polylink/offerMicroservice/repositories/OfferRepository.java
@@ -4,17 +4,18 @@ import com.polylink.offerMicroservice.entity.Offer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface OfferRepository extends JpaRepository<Offer, Integer> , CrudRepository<Offer, Integer> {
 
      List<Offer> findAll();
 
-     List<Offer> findOffersByIdEmployer(Integer id_employer);
+     List<Offer> findOffersByIdEmployer(Long id_employer);
 
      Offer findOfferById(Integer id_offer);
 
      Offer save(Offer offer);
-
+     @Transactional
      void deleteOfferById(Integer id_offer);
 }
